c61e514bf718e9d0848d1644fac2bd02a2cd8504
wiscoming2 <47865416+wiscoming2@users.noreply.github.com>
2019-02-26 18:40:00 +0200

    Update and rename caculetor.js to 25.02caculetor.js

--- 25.02caculetor.js
+++ 25.02caculetor.js
@@ -10,6 +10,7 @@
         <button onclick="multiply()">multiply</button>
         <button onclick="minus()">minus</button>
         <button onclick="divide()">divide</button>
+        <button onclick="WHOSbigger()">WHOSbigger</button>
     </body>
     <script>
         function add()
@@ -34,7 +35,34 @@
         {
             var a = parseInt($('#ainput').val())
             var b = parseInt($('#binput').val())
-            alert(a / b)
+            if(b > 0)
+            {
+                alert(a / b)
+            }
+            else
+            {
+                alert("cannot divide by zero")
+            }
+        }
+        function WHOSbigger()
+        {
+            var a = parseInt($('#ainput').val())
+            var b = parseInt($('#binput').val())
+            if(a == b)
+            {
+                alert ("equal")
+            }
+            else
+            {
+                if(a > b)
+                {
+                    alert("a is bigger")
+                }
+                else
+                {
+                    alert("b is bigger")
+                }
+            }
         }
     </script>
 </html>
