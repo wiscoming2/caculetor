0db593f08c2250b3c558861f8409a347603f435a
wiscoming2 <47865416+wiscoming2@users.noreply.github.com>
2019-02-26 18:21:16 +0200

    Update caculetor.js

--- caculetor.js
+++ caculetor.js
@@ -1,17 +1,40 @@
 <html>
     <head>
-        <body>
-            <p> a = <input type="number" id="anumber"/></p>
-            <p> b = <input type="number" id="bnumber"/></p>
-            <button onclick =alert(add(anumber,bnumber))>add</button>
-        </body>
-        <script>
-            
-
-            function add(a1 , b1)
-            {
-                return parseInt(a1) + parseInt(b1)
-            }
-        </script>
+        <title>html rocks!!</title>
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"> </script> 
     </head>
+    <body>
+        <p> a =<input type="number" id="ainput"/></p>
+        <p> b =<input type="number" id="binput"/></p>
+        <button onclick="add()">add</button>
+        <button onclick="multiply()">multiply</button>
+        <button onclick="minus()">minus</button>
+        <button onclick="divide()">divide</button>
+    </body>
+    <script>
+        function add()
+        {
+            var a = parseInt($('#ainput').val())
+            var b = parseInt($('#binput').val())
+            alert(a + b)
+        }
+        function multiply()
+        {
+            var a = parseInt($('#ainput').val())
+            var b = parseInt($('#binput').val())
+            alert(a * b)
+        }
+        function minus()
+        {
+            var a = parseInt($('#ainput').val())
+            var b = parseInt($('#binput').val())
+            alert(a - b)
+        }
+        function divide()
+        {
+            var a = parseInt($('#ainput').val())
+            var b = parseInt($('#binput').val())
+            alert(a / b)
+        }
+    </script>
 </html>
